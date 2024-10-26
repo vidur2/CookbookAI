1c1bb50b3b90a4c2a75c381dc83c240e4a682870
vidur2 <vmod2005@gmail.com>
2024-10-25 22:19:07 -0400

    img to recipe done

--- api_exploration/llama-index-claude.py
+++ api_exploration/llama-index-claude.py
@@ -3,35 +3,38 @@ import dotenv
 import httpx
 import base64
 
-image1_url = "https://upload.wikimedia.org/wikipedia/commons/a/a7/Camponotus_flavomarginatus_ant.jpg"
-image1_media_type = "image/jpeg"
-image1_data = base64.b64encode(httpx.get(image1_url).content).decode("utf-8")
 
-dotenv.load_dotenv()
+def imgToRecipe(b64Url, media_type):
+    image1_url = "https://t4.ftcdn.net/jpg/01/33/97/33/360_F_133973378_UVcL2YBMV6bzaZTEE6rfVeEcIHZpRDIl.jpg"
+    media_type = "image/jpeg"
+    image1_data = b64Url.decode("utf-8")
 
-client = anthropic.Anthropic()
+    dotenv.load_dotenv()
 
-message = client.messages.create(
-    model="claude-3-5-sonnet-20241022",
-    max_tokens=1024,
-    messages=[
-        {
-            "role": "user",
-            "content": [
-                {
-                    "type": "image",
-                    "source": {
-                        "type": "base64",
-                        "media_type": image1_media_type,
-                        "data": image1_data,
+    client = anthropic.Anthropic()
+
+    message = client.messages.create(
+        model="claude-3-5-sonnet-20241022",
+        max_tokens=1024,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": media_type,
+                            "data": image1_data,
+                        },
                     },
-                },
-                {
-                    "type": "text",
-                    "text": "Describe this image."
-                }
-            ],
-        }
-    ],
-)
-print(message)
+                    {
+                        "type": "text",
+                        "text": "Make a recipe out of the ingredients that you see in this image. Just say the recipe, no preamble."
+                    }
+                ],
+            }
+        ],
+    )
+
+    return message.content[0].text
