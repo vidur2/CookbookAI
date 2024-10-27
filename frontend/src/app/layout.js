1176702dd33b84210b48a01306e537c58fd54a03
vidur2 <vmod2005@gmail.com>
2024-10-27 04:14:46 -0400

    changed icon

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -3,6 +3,21 @@ import ThemeRegistry from './ThemeRegistry';
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel='icon' href='/favicon.ico' sizes='any'></link>
+        <link
+          rel="icon"
+          href="/icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+        <link
+          rel="apple-touch-icon"
+          href="/apple-icon?<generated>"
+          type="image/<generated>"
+          sizes="<generated>"
+        />
+      </head>
       <body>
         <ThemeRegistry>
           {children}
